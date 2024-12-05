27cfc9eb20283a6a419e69071a6651ab970be9e0
Marco Madera <marcoseguro2@gmail.com>
2024-12-05 05:22:42 -0700

    add classname in link (#19)

--- components/ALink.tsx
+++ components/ALink.tsx
@@ -18,11 +18,12 @@ export function ALink(
   >
 ): ReactElement {
   const { variant, ...rest } = props;
+  const classNames = [styles.anchor, variant && styles.action, props.className]
+    .filter(Boolean)
+    .join(" ");
+
   return (
-    <Link
-      {...rest}
-      className={`${styles.anchor} ${variant ? styles.action : ""}`}
-    >
+    <Link {...rest} className={classNames}>
       {props.children}
     </Link>
   );
