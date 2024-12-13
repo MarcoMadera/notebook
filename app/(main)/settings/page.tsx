6143f14b38d08cbc26edc4dc54ead66ff757b4bf
Marco Madera <marcoseguro2@gmail.com>
2024-12-13 16:53:26 -0700

    settings sidebar (#35)

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -1,12 +1,42 @@
 import { ReactElement } from "react";
 
-import { TestButton } from "./testButton";
+import { Font, Lock, Logout, Sun } from "@/app/ui/icons";
+import { Divider } from "@/components/Divider";
+import styles from "@/components/SidebarAllNotes.module.css";
+import { SideBarItem } from "@/components/SideBarItem";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export default function Home(): ReactElement {
   return (
-    <div>
-      <p>Settings</p>
-      <TestButton />
+    <div className="grid-container grid-container--no-sidebar">
+      <aside
+        className={
+          "col-span-3 col-start-1 tablet:col-span-8 tablet:col-start-1 mobile:col-span-8 "
+        }
+      >
+        <ul className={`${styles.container} ${styles.group}`}>
+          <SideBarItem href="/settings/theme" icon={<Sun />}>
+            Color Theme
+          </SideBarItem>
+          <SideBarItem href="/settings/theme" icon={<Font />}>
+            Font Theme
+          </SideBarItem>
+          <SideBarItem href="/settings/theme" icon={<Lock />}>
+            Change Password
+          </SideBarItem>
+          <Divider />
+          <SideBarItem icon={<Logout />} variant="button" type="button">
+            Logout
+          </SideBarItem>
+        </ul>
+      </aside>
+      <main
+        className={
+          "col-span-6 col-start-4 tablet:col-span-8 tablet:col-start-1 mobile:col-span-8"
+        }
+      >
+        <ThemeToggle />
+      </main>
     </div>
   );
 }
--- components/SideBarItem.tsx
+++ components/SideBarItem.tsx
@@ -1,36 +1,83 @@
-import type { PropsWithChildren, ReactElement, ReactNode } from "react";
+import type {
+  ButtonHTMLAttributes,
+  PropsWithChildren,
+  ReactElement,
+  ReactNode,
+} from "react";
 
 import { ALink } from "./ALink";
+import anchorStyles from "./ALink.module.css";
 import styles from "./SideBarItem.module.css";
 
 import { ChevronRight } from "@/app/ui/icons";
 
-export function SideBarItem({
-  children,
-  selected,
-  icon,
-  href,
-}: PropsWithChildren<{
-  href: string;
+type BaseProps = {
   selected?: boolean;
   icon?: ReactNode;
-}>): ReactElement {
+};
+
+type LinkProps = BaseProps & {
+  variant?: "link";
+  href: string;
+};
+
+type ButtonProps = BaseProps & {
+  variant: "button";
+  href?: never;
+} & Omit<ButtonHTMLAttributes<HTMLButtonElement>, keyof BaseProps | "href">;
+
+type SideBarItemProps = LinkProps | ButtonProps;
+
+function isLinkProps(props: SideBarItemProps): props is LinkProps {
+  return props.variant === "link";
+}
+
+export function SideBarItem(
+  props: PropsWithChildren<SideBarItemProps>
+): ReactElement {
+  const { children, selected, icon } = props;
+  const commonClassNames = `${styles.container} ${selected ? styles.selected : ""}`;
+
+  const content = (
+    <>
+      <div className={styles.icon}>{icon}</div>
+      <div className={`${styles.content} text-preset-4`}>{children}</div>
+      {selected ? (
+        <div className={styles.rightIcon}>
+          <ChevronRight viewBox="8 1 24 24" />
+        </div>
+      ) : (
+        <div className={styles.rightIcon}></div>
+      )}
+    </>
+  );
+
+  if (!props.variant || isLinkProps(props)) {
+    return (
+      <li className={styles.listItem}>
+        <ALink href={props.href} className={commonClassNames}>
+          {content}
+        </ALink>
+      </li>
+    );
+  }
+
+  const {
+    variant: _,
+    href: __,
+    selected: ___,
+    icon: ____,
+    ...buttonProps
+  } = props;
   return (
     <li className={styles.listItem}>
-      <ALink
-        href={href}
-        className={`${styles.container} ${selected ? styles.selected : ""}`}
+      <button
+        {...buttonProps}
+        className={`${commonClassNames} ${styles.button} ${anchorStyles.anchor}`}
+        type={buttonProps.type ?? "button"}
       >
-        <div className={styles.icon}>{icon}</div>
-        <div className={`${styles.content} text-preset-4`}>{children}</div>
-        {selected ? (
-          <div className={styles.rightIcon}>
-            <ChevronRight viewBox="8 1 24 24" />
-          </div>
-        ) : (
-          <div className={styles.rightIcon}></div>
-        )}
-      </ALink>
+        {content}
+      </button>
     </li>
   );
 }
